483cc48938fcf2efc388df178119bdf0ee277005
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-05-17 17:16:50 -0300

    fim dos video do desafio

---
+++ src/main/java/org/banco/Conta.java
@@ -0,0 +1,52 @@
+package org.banco;
+
+public abstract class Conta implements IConta {
+    private static int SEQUENCIAL = 1;
+    protected int AGENCIA_PADRAO = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    private Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.printf("Titlular: %s%n", this.cliente.getNome());
+        System.out.printf("Agencia: %d%n", this.agencia);
+        System.out.printf("Numero: %d%n", this.numero);
+        System.out.printf("Saldo: %.2f%n", this.saldo);
+    }
+}
